1e7b4b5e6669a1e212d65978cf3e61d8c1b70140
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-01-12 23:23:44 +0300

    Initial Commit

---
+++ SwiftUIDay16/SwiftUIDay16App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDay16App.swift
+//  SwiftUIDay16
+//
+//  Created by Fatih Durmaz on 12.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDay16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
