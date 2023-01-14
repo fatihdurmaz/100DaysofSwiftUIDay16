c11c3fc9268b6d898f39f06cf081174e39462c16
Fatih Durmaz <71706970+fatihdurmaz@users.noreply.github.com>
2023-01-14 14:19:00 +0300

    xcode ctrl + i kod hizalama

--- SwiftUIDay16/ContentView.swift
+++ SwiftUIDay16/ContentView.swift
@@ -15,26 +15,26 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             Form{
-                    Picker("Öğrenci Seçiniz", selection: $seciliOgrenci) {
-                        /*Picker nesnesi seçili değerin ne olduğunu selection: ($değer) ile alır. $ işareti ile bu değerin iki yönlü (two way binding) çalışacağını bildirir.
-                         Yani bu değer değişirse view'e bilgi vermeliyizki değerin kullanıldığı yerler, değer değiştiğinde bundan haberdar olabilsin.
-                         */
+                Picker("Öğrenci Seçiniz", selection: $seciliOgrenci) {
+                    /*Picker nesnesi seçili değerin ne olduğunu selection: ($değer) ile alır. $ işareti ile bu değerin iki yönlü (two way binding) çalışacağını bildirir.
+                     Yani bu değer değişirse view'e bilgi vermeliyizki değerin kullanıldığı yerler, değer değiştiğinde bundan haberdar olabilsin.
+                     */
+                    
+                    ForEach(ogrenciler, id:\.self){ogrenci in
+                        /* Veriler bir veri kaynağından (burada öğrenciler dizisinden) geliyor. Hangi değerle ilgli işlem yapmamızı ayırt etmesi için id: bizden bunu belirtmemizi istiyor. /.self diyerek dizinin elemanlarını gösteriyoruz. Benzersiz oldukları için sorun yok. Başka veri kaynağımız olduğu zaman (diyelim ki veritabanı veya json dosyası) UUID gibi benzersiz bir değer de verebilirdik. */
                         
-                        ForEach(ogrenciler, id:\.self){ogrenci in
-                            /* Veriler bir veri kaynağından (burada öğrenciler dizisinden) geliyor. Hangi değerle ilgli işlem yapmamızı ayırt etmesi için id: bizden bunu belirtmemizi istiyor. /.self diyerek dizinin elemanlarını gösteriyoruz. Benzersiz oldukları için sorun yok. Başka veri kaynağımız olduğu zaman (diyelim ki veritabanı veya json dosyası) UUID gibi benzersiz bir değer de verebilirdik. */
-                            
-                            HStack { // picker in içini simge ve öğrenciler dizisinin elemanlarıyla for döngüsü yardımıyla dolduruyoruz.
-                                Text(ogrenci)
-                                Image(systemName: "person")
-                            }
+                        HStack { // picker in içini simge ve öğrenciler dizisinin elemanlarıyla for döngüsü yardımıyla dolduruyoruz.
+                            Text(ogrenci)
+                            Image(systemName: "person")
                         }
                     }
+                }
             }
             .navigationTitle("Picker, Form ve Two-Way Binding")
             .navigationBarTitleDisplayMode(.inline)
         }
-        }
     }
+}
 
 
 struct ContentView_Previews: PreviewProvider {
